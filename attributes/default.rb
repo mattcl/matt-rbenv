d838e903456d59588409ace5915b0b0c90ea4e83
Matt Chun-Lum <mchunlum@gmail.com>
2014-03-25 21:26:31 -0700

    use default instead of override

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-override['rbenv']['group_users'] = ['vagrant']
+default['rbenv']['group_users'] = ['vagrant']
 
 default['matt-rbenv']['rubies'] = ['1.9.3-p545','2.0.0-p451']
 default['matt-rbenv']['global'] = '2.0.0-p451'
